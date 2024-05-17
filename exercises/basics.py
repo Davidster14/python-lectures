4a021a35a6b26380f942c0fa6cf2a3f80df61249
Davidster14 <149767648+Davidster14@users.noreply.github.com>
2024-05-17 14:55:16 +0200

    xyz

--- exercises/basics.py
+++ exercises/basics.py
@@ -3,68 +3,115 @@ Schreibe je eine Funtion add, subtract, multiply, divide, die die
 jeweilige Grundrechenart auf die beiden übergebenen Parameter A und B
 anwendet.
 '''
-def add(a, b): pass
-# ...
-# ...
-# ...
+def add(a, b): 
+    return a + b
+
+def subtract(a, b):     
+    return a - b
+
+def multiply(a, b): 
+    return a * b 
+
+def divide(a, b): 
+    return a / b 
+
+
+print(add(12, 24))  
+
+
+print(subtract(112, 44))  
+
+print(multiply(77, 3))  
+
+
+print(divide(99, 33))  
 
 '''
 Schreibe eine Funktion, die eine Temperatur in Celsius in eine Temperatur in
 Fahrenheit umrechnet.
 '''
 def inFahrenheit(celsius):
-    '''
-    :param celsius: Temperature to convert in Celsius
-    :type celsius: float
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
-    :return: Converted temperature in Fahrenheit
-    '''
-    pass
+#beispielberechnungen
+print (inFahrenheit(28)) 
+       
+print (inFahrenheit(15))
+    
 
 '''
-Schreibe eine Funktion, die eine Temeratur in Fahrenheit in eine Temperatur
+Schreibe eine Funktion, die eine Temperatur in Fahrenheit in eine Temperatur
 in Celsius umrechnet.
 '''
 def inCelsius(fahrenheit):
-    '''
+
+    celsius = (fahrenheit - 32) * 5/9 
+    return celsius
+
+
+print (inCelsius(83)) 
+
+print (inCelsius(60)) 
+#Versuch        
+#print(f"{inCelsius}(58) celsius")
+    
+
+'''
     :param fahrenheit: Temperature to convert in Fahrenheit
     :type fahrenheit: float
 
     :return: Converted temperature in Celsius
     :rtype: float
-    '''
-    pass
 
-'''
+______________________________________________________________
+
 Schreibe eine Funktion, die prüft, ob eine Zahl gerade ist.
 '''
-def isEven():
+def ist_gerade(zahl):
+    return zahl % 2 == 0
+
+
+zahl = 22
+if ist_gerade(zahl):
+    print(f"{zahl} ist gerade.")
+else:
+    print(f"{zahl} ist ungerade.")
+
     '''
     :param num: Number to test
     :type num: int
 
     :rtype: bool
     '''
-    pass
+    
 
 '''
 Schreibe eine Funktion, die prüft, ob eine Zahl ungerade ist.
 '''
-def isOdd():
+def ist_ungerade(zahl):
+    return zahl % 2 != 0
+
+
+zahl = 43
+if ist_ungerade(zahl):
+    print(f"{zahl} ist ungerade.")
+else:
+    print(f"{zahl} ist gerade.")
     '''
     :param num: Number to test
     :type num: int
 
     :rtype: bool
     '''
-    pass
+    
 
 # Kontrollfluss
 
 # if
 
 '''
-Schreibe eine Funkntion, die abhängig von dem als Zahl eingegebenen Monat die
+Schreibe eine Funktion, die abhängig von dem als Zahl eingegebenen Monat die
 passende Jahreszeit zurückgibt. Und zwar
 
 "Frühling" für die Monate März, April, Mai
@@ -73,12 +120,27 @@ passende Jahreszeit zurückgibt. Und zwar
 "Winter" für die Monate Dezember, Januar und Februar.
 '''
 def jahreszeit(monat):
-    '''
+    if monat in [3, 4, 5]:
+        return "Frühling"
+    elif monat in [6, 7, 8]:
+        return "Sommer"
+    elif monat in [9, 10, 11]:
+        return "Herbst"
+    elif monat in [12, 1, 2]:
+        return "Winter"
+    else:
+        return "Ungültiger Monat bitte wähle zwischen 1 - 12 aus. "
+
+
+monat = 42
+print(f"Der {monat}. Monat  gehört zur Jahreszeit: {jahreszeit(monat)}")
+
+'''
     :type monat: int
     :return: Jahreszeit
     :rtype: string
     '''
-    pass
+#____________________________________________________________________________
 
 '''
 Schreibe eine Funktion, die die Umsatzsteuer anhand des Umsatzes und des
@@ -87,8 +149,31 @@ der Freigrenze von 17.500 EUR (für die Steuerjahre 2003-2019) bzw. 22.000 EUR
 (für die Steuerjahre ab 2020) soll die Kleinunternehmerregelung angewendet
 und keine Umsatzsteuer berechnet werden. Der Rückgabewert ist dann 0.
 '''
-def umsatzsteuer(umsatz, steuerjahr = 2024):
-    '''
+#version 0.1 ->  Freigrenze beachten!
+def umsatzsteuer(umsatz, steuerjahr):
+    #  Freigrenzen sind
+    if steuerjahr >= 2020:
+        freigrenze = 22000
+    else:
+        freigrenze = 17500
+    
+    # Kleinunternehmerregelung möglich?
+    if umsatz < freigrenze:
+        return 0
+    else:
+        # Berechnung der Umsatzsteuer
+        steuersatz = 0.19
+        umsatzsteuer = umsatz * steuersatz
+        return umsatzsteuer
+
+# Umsatz und Steuerjahr  werden eingefügt 
+umsatz = 26000
+steuerjahr = 2007
+umsatzsteuer = umsatzsteuer(umsatz, steuerjahr)
+print(f"Die Umsatzsteuer bei einen Umsatz von {umsatz} EUR im Jahr {steuerjahr} beträgt: {umsatzsteuer:.2f} EURO")
+
+
+'''
     :param umsatz: Umsatz im Steuerjahr
     :type umsatz: float
     :param steuerjahr: Steuerjahr
@@ -97,7 +182,7 @@ def umsatzsteuer(umsatz, steuerjahr = 2024):
     :return: Umsatzsteuer
     :rtype: float
     '''
-    pass
+    
 
 # match
 
